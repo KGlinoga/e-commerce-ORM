f23e51b950a7bae534601a758b090720216884d1
Kristina Faye <kglinoga@gmail.com>
2022-08-01 19:33:11 -0700

    updated Category id

--- models/Category.js
+++ models/Category.js
@@ -7,6 +7,16 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
+    category_id: {
+      type:DataTypes.INTEGER,
+      null: false,
+      primarykey: true,
+      auto_increment: true,
+    },
+     category_name: {
+      type:DataTypes.STRING,
+      null: false
+     }
   },
   {
     sequelize,
