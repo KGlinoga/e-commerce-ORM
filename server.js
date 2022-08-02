6c48ccb0212716ec2902846712b7f1ddd48c776c
Kristina Faye <kglinoga@gmail.com>
2022-08-01 19:26:59 -0700

    updated server.js

--- server.js
+++ server.js
@@ -1,17 +1,35 @@
 const express = require('express');
-const routes = require('./routes');
+// changed to match mini proj, pls advise lol
+const allRoutes = require('./routes');
 // import sequelize connection
-
+const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// require our models for syncing:
+const {Category, Product, ProductTag, Tag} = require('./models');
 
-app.use(routes);
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// this differs from the mini-proj!  in that it was:
+app.use('/',allRoutes);   
+// AND line 2 was allRoutes = require('controllers'); this is def worth asking about.
+// app.use(routes); <~~OG starter code
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+
+sequelize.sync({ force: false }).then(function() {
+  app.listen(PORT, function() {
+      console.log('App listening on PORT ' + PORT `AKA http://localhost:${PORT}`);
+  });
+}).catch(err=>{
+  console.log(err)
 });
+
+// OG starter code:
+// app.listen(PORT, () => {
+//   console.log(`App listening on port ${PORT}!`);
+// });
